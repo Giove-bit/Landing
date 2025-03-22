114ffe6e0694589b3d296c827319ae02dc996f04
Giovanni Corigliano <gianni94@hotmail.it>
2025-03-22 16:04:14 +0100

    added bad request

--- backend-spring/src/main/java/com/example/demo/controller/AuthController.java
+++ backend-spring/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
+
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,13 +19,17 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestHeader("X-Correlation-ID") String correlationId, @RequestBody Map<String, String> creds) {
         MDC.put("sid", correlationId);
         if ("user".equals(creds.get("username")) && "pass".equals(creds.get("password"))) {
-            Map<String, String> res = Map.of("token", "dummy-jwt-token");
+            String token = UUID.randomUUID().toString();
+            Map<String, String> res = new HashMap<>();
+            res.put("user", creds.get("username"));
+            res.put("token", token);
             log.info("Request: {} - Response: {}", creds, res);
             log.info("Login OK");
             MDC.clear();
             return ResponseEntity.ok(res);
 
+        } else {
+            return ResponseEntity.badRequest().body("Invalid credentials");
         }
-        throw new RuntimeException("Invalid credentials");
     }
 }
