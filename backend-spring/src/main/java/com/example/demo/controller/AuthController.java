1b70b300bf823ee386eaefb43d4eacfe07a84e70
Giovanni Corigliano <gianni94@hotmail.it>
2025-03-22 12:19:45 +0100

    refactor backend

--- backend-spring/src/main/java/com/example/demo/controller/AuthController.java
+++ backend-spring/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin
+@Slf4j
 public class AuthController {
+
     @PostMapping("/login")
     public Map<String, String> login(@RequestBody Map<String, String> creds) {
         if ("user".equals(creds.get("username")) && "pass".equals(creds.get("password"))) {
+            log.info("Login OK");
             return Map.of("token", "dummy-jwt-token");
+
         }
         throw new RuntimeException("Invalid credentials");
     }
