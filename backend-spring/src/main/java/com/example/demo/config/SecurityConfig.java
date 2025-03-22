f4cd555cdf4b87d5985fcd95ed7ecf48ef68ef71
Giovanni Corigliano <gianni94@hotmail.it>
2025-03-22 10:53:43 +0100

    added security config

---
+++ backend-spring/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // disabilita CSRF (necessario per POST da frontend)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // o .formLogin().disable()
+
+        return http.build();
+    }
+}
